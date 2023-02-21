5d4aa514f4ac118a8b25eaa13b85a8e6ac5df6d3
buterajacques1 <jbutera702@gmail.com>
2023-02-21 22:22:36 +0200

    MySQL setup test

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,16 +1,11 @@
--- Prepares a MySQL server for the project, creating a Database and user
+-- Create the database hbnb_test_db, but only if it does not already exist
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
--- Create a database with the name 'hbnb_dev_db', but only if it doesn't already exist.
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- Create the user hbnb_test on localhost, but only if it does not already exist
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
--- Create a user with the name 'hbnb_dev' and host 'localhost', if it doesn't already exist.
--- Set the password for the user to 'hbnb_dev_pwd'.
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- Grant all privileges on the hbnb_test_db database to the hbnb_test user, but only if it exists
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 
--- Grant all privileges on the 'hbnb_dev_db' database to the 'hbnb_dev' user, but only for connections from 'localhost'.
--- This means that the 'hbnb_dev' user can read and write data to this database, but only if they are connecting from the same machine that the MySQL server is running on.
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-
--- Grant SELECT privilege on the 'performance_schema' database to the 'hbnb_dev' user, but only for connections from 'localhost'.
--- This means that the 'hbnb_dev' user can only read data from this database, but only if they are connecting from the same machine that the MySQL server is running on.
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+-- Grant the SELECT privilege on the performance_schema database to the hbnb_test user, but only if it exists
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
