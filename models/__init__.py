2079ceb8b4ac67776e38c1f68f7987063abe5d12
buterajacques1 <jbutera702@gmail.com>
2023-02-24 00:41:23 +0200

    DBStorage - Place

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import os
 
 if os.environ.get('HBNB_TYPE_STORAGE') == 'db':
--- models/state.py
+++ models/state.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
