3321dacd7cb8e445efc4902404ae9d8a1016d10b
buterajacques1 <jbutera702@gmail.com>
2023-03-10 22:30:13 +0200

    prepare your servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -10,6 +10,6 @@ sudo mkdir -p /data/web_static/shared/
 sudo echo "Best School" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '57 i \\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx restart
 exit 0
