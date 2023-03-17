ce73fda826735c228b0c43e0069fc299069a7312
buterajacques1 <jbutera702@gmail.com>
2023-03-17 16:21:10 +0200

    Is it a number?

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,52 +1,43 @@
 #!/usr/bin/python3
+""" Module to start a web applicaction and
+    display binded messages
 """
-Starts a Flask web application with the following routes:
-- /: display "Hello HBNB!"
-- /hbnb: display "HBNB"
-- /c/<text>: display "C " followed by the value of the text variable (replace
-underscore _ symbols with a space)
-- /python/(<text>): display "Python " followed by the value of the text variable
-(replace underscore _ symbols with a space). The default value of text is "is
-cool"
-- /number/<n>: display "<n> is a number" only if n is an integer
-"""
+
 
 from flask import Flask
-
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """Displays 'Hello HBNB!'"""
+def hello():
+    """Function to return a greeting"""
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Displays 'HBNB'"""
+    """Function to return a HBNB at /hbnb url bind"""
     return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c(text):
-    """Displays 'C ' followed by the value of the text variable"""
-    return 'C {}'.format(text.replace('_', ' '))
+def c_message(text):
+    """Function to return a message with c"""
+    return "C {}".format(text.replace('_', ' '))
 
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python(text):
-    """Displays 'Python ' followed by the value of the text variable. The default
-    value of text is 'is cool'"""
-    return 'Python {}'.format(text.replace('_', ' '))
+def python_message(text):
+    """Function to return a message with python"""
+    return "Python {}".format(text.replace('_', ' '))
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
-    """Displays '<n> is a number' only if n is an integer"""
-    return '{} is a number'.format(n)
+    """Function to return a number with message"""
+    return "{} is a number".format(n)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
